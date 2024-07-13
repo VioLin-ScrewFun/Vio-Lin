9dfc9105d49a53dcde776d721692a571545c31fd
VioLin-ScrewFun <linar_85@hotmail.com>
2024-07-13 19:01:14 +0200

    Update and rename scripts.js to Scripts

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleVideo() {
-  // <video id="background-video" autoplay loop class="video-background">
-         <source src="lasti.mp4" type="video/mp4">
-
-  console.log("Toggling video...");
-}
