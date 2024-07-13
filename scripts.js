3f626fa7ef29b0ac5f738cd23f24552345220900
VioLin-ScrewFun <linar_85@hotmail.com>
2024-07-13 18:37:04 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleVideo() {
+  // <video id="background-video" autoplay loop class="video-background">
+         <source src="/Users/Lina/Desktop/Github/VioLin/web/lasti/lasti.mp4" type="video/mp4">
+
+  console.log("Toggling video...");
+}
